7aa9f82a0e5650cdedf01ab8a2d202e6eade0210
Corfu Denisa-Elena <corfudenisa@gmail.com>
2024-01-07 19:24:19 +0200

    da

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 import pathlib
 import random
 from string import ascii_letters
+from rich.console import Console
+
+console = Console()
 
 
 def getWords(leng):
@@ -8,23 +11,29 @@ def getWords(leng):
         old_list = file.read().splitlines()
     new_list = [word.lower() for word in old_list if len(word) == leng]
     return new_list
+
+
 def checkWord(word) -> bool:
-    if(len(list(word))!=5):
+    if len(list(word)) != 5:
         print("YOUR WORD MUST HAVE 5 LETTERS")
         return False
-    Letters=[let for let in word]
-    ASCII_LETTERS = {chr(i): i for i in range(ord('A'), ord('Z')+1)}
+    Letters = [let for let in word]
+    ASCII_LETTERS = {chr(i): i for i in range(ord("A"), ord("Z") + 1)}
     for let in Letters:
         if let not in ASCII_LETTERS:
             print(f"{let} IS NOT ALLOWED")
             return False
     return True
+
+
 def processWord(word):
-    #word = get_random_word()
+    # word = get_random_word()
     randWord = "SNAKE"
     word = word.upper
     if checkWord(word):
         print("Good")
+
+
 def get_random_word():
     wordlist = getWords(5)
     words = [
@@ -38,3 +47,7 @@ def get_random_word():
 def game_over(word):
     print(f"The word was {word}")
 
+
+def refresh_page(headline):
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-processWord("Value")
-from functions import get_random_word
+from functions import processWord, get_random_word, game_over, refresh_page
 
 word = get_random_word()
-print(word)
+
+for guess_num in range(1, 7):
+    guess = input(f"\nGuess {guess_num}: ")
+    while processWord(guess, word):
+        _ = _
+    if guess == word:
+        print("GOOD JOB!\n")
+        break
+    else:
+        game_over()
