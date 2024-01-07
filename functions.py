9b9588b09a580598d87bf70330af6ce52263829a
Paun Darius <paundariualexandru@gmail.com>
2024-01-07 14:56:25 +0200

    dece?

--- functions.py
+++ functions.py
@@ -3,4 +3,24 @@ def getWords(leng):
         old_list = file.read().splitlines()
     new_list = [word.lower() for word in old_list if len(word) == leng]
     return new_list
+def checkWord(word) -> bool:
+    if(len(list(word))!=5):
+        print("YOUR WORD MUST HAVE 5 LETTERS")
+        return False
+    Letters=[let for let in word]
+    ASCII_LETTERS = {chr(i): i for i in range(ord('A'), ord('Z')+1)}
+    for let in Letters:
+        if let not in ASCII_LETTERS:
+            print(f"{let} IS NOT ALLOWED")
+            return False
+    return True
 
+
+def get_random_word():#Denisa
+    return 0
+def processWord(word):
+    #word = get_random_word()
+    randWord = "SNAKE"
+    word = word.upper
+    if checkWord(word):
+        print("Good")
